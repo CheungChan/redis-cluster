9032293cc0435307ef2ebb80f617c568ee3739be
香饽饽 <1377699408@qq.com>
2019-12-17 14:52:18 +0800

    init

--- restart.sh
+++ restart.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-MASTER_PORT=10001
-NODE_PORT=10002
-SENTINEL_PORT=10003
+export MASTER_PORT=10001
+export NODE_PORT=10002
+export SENTINEL_PORT=10003
 
 git pull
 
@@ -22,17 +22,10 @@ for d in ${dir}; do
 done
 
 docker rm -f redis-sentinel redis-slave redis-master
-docker rm -f redis-sentinel redis-slave redis-master
 
 # 主redis，该容器命名为redis-master 使用mynetwork网络 端口映射10001 对应容器内部端口的6379 指定容器固定IP:172.10.0.2 使用redis镜像来生成容器并在后台运行
-docker run -it  --name redis-master -v ${PWD}/master/redis.conf:/etc/redis.conf \
-  -v /data/redis/${MASTER_PORT}:/data \
-  -d -p ${MASTER_PORT}:6379 --net mynetwork --restart=always --ip 172.10.0.2 redis "redis-server /etc/redis.conf"
+docker run -it  --name redis-master -v ${PWD}/master/redis.conf:/etc/redis.conf  -v /data/redis/${MASTER_PORT}:/data  -p ${MASTER_PORT}:6379 --net mynetwork --restart=always --ip 172.10.0.2  -d  redis "redis-server /etc/redis.conf"
 # 从redis，该容器命名为redis-slave  使用mynetwork网络 端口映射10002对应容器内部端口的6379 指定容器固定IP:172.10.0.3 使用redis镜像来生成容器并在后台运行
-docker run -it  --name redis-slave -v ${PWD}/slave/redis.conf:/etc/redis.conf \
-  -v /data/redis/${NODE_PORT}:/data \
-  -d -p ${NODE_PORT}:6379 --net mynetwork --restart=always --ip 172.10.0.3 redis "redis-server /etc/redis.conf"
+docker run -itd  --name redis-slave -v ${PWD}/slave/redis.conf:/etc/redis.conf -v /data/redis/${NODE_PORT}:/data  -p ${NODE_PORT}:6379 --net mynetwork --restart=always --ip 172.10.0.3 redis "redis-server /etc/redis.conf"
 # 哨兵redis，该容器命名为redis-sentinel  使用mynetwork网络 端口映射10003对应容器内部端口的6379 指定容器固定IP:172.10.0.4 使用redis镜像来生成容器并在后台运行
-docker run -it  --name redis-sentinel -v ${PWD}/sentinel/sentinel.conf:/etc/sentinel.conf \
-  -v /data/redis/${SENTINEL_PORT}:/data \
-  -d -p ${SENTINEL_PORT}:26379 --net mynetwork --restart=always --ip 172.10.0.4 redis "redis-sentinel /etc/sentinel.conf"
+docker run -itd  --name redis-sentinel -v ${PWD}/sentinel/sentinel.conf:/etc/sentinel.conf  -v /data/redis/${SENTINEL_PORT}:/data -p ${SENTINEL_PORT}:26379 --net mynetwork --restart=always --ip 172.10.0.4 redis "redis-sentinel /etc/sentinel.conf"
