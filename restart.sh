78672a4fac9eb2eda66c625f218e0a5a32a19927
香饽饽 <1377699408@qq.com>
2019-12-17 14:15:45 +0800

    init

--- restart.sh
+++ restart.sh
@@ -7,7 +7,7 @@ SENTINEL_PORT=10003
 dir="/data/redis/${MASTER_PORT} /data/redis/${NODE_PORT} /data/redis/${SENTINEL_PORT}"
 for d in ${dir}; do
   if [ ! -d $d ]; then
-    mkdir $d
+    mkdir -p $d
   else
     echo $d exist
   fi
@@ -22,8 +22,8 @@ docker run -it --name redis-master -v ${PWD}/master/redis.conf:/etc/redis.conf \
 # 从redis，该容器命名为redis-slave  使用mynetwork网络 端口映射10002对应容器内部端口的6379 指定容器固定IP:172.10.0.3 使用redis镜像来生成容器并在后台运行
 docker run -it --name redis-slave -v ${PWD}/slave/redis.conf:/etc/redis.conf \
   -v /data/redis/${NODE_PORT}:/data \
-  -d -p ${NODE_PORT}:6379 --net mynetwork  --restart=always  --ip 172.10.0.3 redis redis-server /etc/redis.conf
+  -d -p ${NODE_PORT}:6379 --net mynetwork --restart=always --ip 172.10.0.3 redis redis-server /etc/redis.conf
 # 哨兵redis，该容器命名为redis-sentinel  使用mynetwork网络 端口映射10003对应容器内部端口的6379 指定容器固定IP:172.10.0.4 使用redis镜像来生成容器并在后台运行
 docker run -it --name redis-sentinel -v ${PWD}/sentinel/sentinel.conf:/etc/sentinel.conf \
   -v /data/redis/${SENTINEL_PORT}:/data \
-  -d -p ${SENTINEL_PORT}:26379 --net mynetwork --restart=always  --ip 172.10.0.4 redis redis-sentinel /etc/sentinel.conf
+  -d -p ${SENTINEL_PORT}:26379 --net mynetwork --restart=always --ip 172.10.0.4 redis redis-sentinel /etc/sentinel.conf
