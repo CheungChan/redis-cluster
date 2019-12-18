ea2233956830b43e926870b0ac778a91a58b6b02
香饽饽 <1377699408@qq.com>
2019-12-18 10:38:42 +0800

    init

---
+++ clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker-compose down
+docker rm -f redis-node-1 redis-node-2 redis-node-3
+docker network disconnect -f redis-cluster_mynetwork redis-node-1
+docker network disconnect -f redis-cluster_mynetwork redis-node-2
+docker network disconnect -f redis-cluster_mynetwork redis-node-3
+docker-compose up
