c6de7dd50413ca2aaf1460beff4a3f75679c6b15
香饽饽 <1377699408@qq.com>
2019-12-18 10:39:51 +0800

    init

--- clean.sh
+++ clean.sh
@@ -5,4 +5,4 @@ docker rm -f redis-node-1 redis-node-2 redis-node-3
 docker network disconnect -f redis-cluster_mynetwork redis-node-1
 docker network disconnect -f redis-cluster_mynetwork redis-node-2
 docker network disconnect -f redis-cluster_mynetwork redis-node-3
-docker-compose up
+docker-compose down
